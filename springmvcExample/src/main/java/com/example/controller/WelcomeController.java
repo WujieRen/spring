ffd42f150c08beca3eaac0097e083960fb41773d
WujieRen <rwj005@outlook.com>
2018-05-08 21:11:33 +0800

    根据控制器Bean的名字将控制器映射到URL：注解方式。

---
+++ springmvcExample/src/main/java/com/example/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by renwujie on 2018/05/08 at 19:25
+ */
+@Controller
+public class WelcomeController {
+    @RequestMapping({"welcome", "wel"})
+    public String welcome() {
+        System.out.println("Welcome");
+        return "";
+    }
+}
